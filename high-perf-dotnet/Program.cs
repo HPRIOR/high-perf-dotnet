380f20230dbd0f6388fb4d63a6a336477dc66e44
Harry Prior <harry.prior@gfk.com>
2021-09-21 12:11:41 +0100

    Initial commit

---
+++ high-perf-dotnet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace high_perf_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var test = new Test();
+            test.test();
+        }
+    }
+
+    ref struct Test
+    {
+        public void test()
+        {
+            foreach (var i in span)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
